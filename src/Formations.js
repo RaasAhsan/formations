710b7ae2348315522218c91742df59362c3d3f28
Muhtasim Ahsan <agro@jantox.com>
2015-04-03 23:08:50 -0500

    fix empty field.

--- src/Formations.js
+++ src/Formations.js
@@ -5,23 +5,31 @@ export var Validations = require('./Validations');
 
 export var validateForm = function(form, fieldValidations) {
   var results = Object.keys(form).map(key => {
-    var validation = fieldValidations[key];
-    if(validation) {
-      var errors = validation.test(form[key], form);
-      var passed = errors.length === 0;
+    if(key && form[key]) {
+      var validation = fieldValidations[key];
+      if(validation) {
+        var errors = validation.test(form[key], form);
+        var passed = errors.length === 0;
 
-      return {
-        field: key,
-        passed: passed,
-        errors: passed ? [] : errors
-      };
+        return {
+          field: key,
+          passed: passed,
+          errors: passed ? [] : errors
+        };
+      } else {
+        console.warn(`A validation for field ${key} wasn't found. Set the validations to an empty array for this field.`);
+
+        return {
+          field: key,
+          passed: true,
+          errors: []
+        };
+      }
     } else {
-      console.warn(`A validation for field ${key} was not found. Set the validations to an empty array for this field.`);
-
       return {
         field: key,
-        passed: true,
-        errors: []
+        passed: false,
+        errors: ["field not found."]
       };
     }
   });
@@ -31,17 +39,16 @@ export var validateForm = function(form, fieldValidations) {
 export var getField = function(target, name) {
   var field = target.querySelector('[name="' + name + '"]');
   if(field) {
-    return (field.value || "");
+    return field.value;
   } else {
-    console.warn(`The field ${name} couldn't be found in the given target. Returning an empty string.`);
-    return "";
+    console.warn(`The field ${name} wasn't found in the given target.`);
+    return null;
   }
 };
 
 export var createForm = function(target, fields) {
   return fields.reduce(function(acc, x) {
-    var field = getField(target, x);
-    if(field)
+    if(x)
       acc[x] = getField(target, x);
     return acc;
   }, {});
